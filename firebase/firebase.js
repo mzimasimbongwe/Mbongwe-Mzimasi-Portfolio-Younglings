6bae5817e9d9daa931f67e6b7cb75fca6105656c
mzimasimbongwe <mzimasimbongwe98@gmail.com>
2023-08-24 10:16:00 +0200

    Trying to conect to database

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,29 +1,27 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
-import {getDatabase,ref, set,push} from "firebase/database"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase, ref, push } from "firebase/database";
 
 // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDHpIVFf_gNoOjlrtbUP1Gv6HyV8HlW4vM",
+    authDomain: "hosting-example-4d431.firebaseapp.com",
+    databaseURL: "https://hosting-1ce05-default-rtdb.firebaseio.com",
+    projectId: "hosting-1ce05",
+    storageBucket: "hosting-1ce05.appspot.com",
+    messagingSenderId: "279380074239",
+    appId: "1:279380074239:web:5d989e8354399f887376ca"
+};
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDHpIVFf_gNoOjlrtbUP1Gv6HyV8HlW4vM",
-//     authDomain: "hosting-example-4d431.firebaseapp.com",
-//     databaseURL: "https://hosting-1ce05-default-rtdb.firebaseio.com",
-//     projectId: "hosting-1ce05",
-//     storageBucket: "hosting-1ce05.appspot.com",
-//     messagingSenderId: "279380074239",
-//     appId: "1:279380074239:web:5d989e8354399f887376ca"
-//     //  measurementId: "G-G7YY9SB8F5"
-//   };
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-// const db = getDatabase();
-//   const app = initializeApp(firebaseConfig);
+export { db }; // Export the database instance
 
-//   export default app;
- 
+// Functions to interact with the database
+const saveData = (data) => {
+  const dataRef = ref(db, "contacts");
+  push(dataRef, data);
+};
+
+export { saveData }; // Export the function to save data
