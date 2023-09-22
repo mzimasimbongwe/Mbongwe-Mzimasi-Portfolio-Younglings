edf0d85227e57b6a2fcb299b92640e43cb17d22b
Mzimasi Mbongwe <82802374+mzimasimbongwe@users.noreply.github.com>
2023-09-22 02:11:57 +0200

    Added duties  on skills

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -106,72 +106,102 @@ const SkillsAndExperience = () => {
           </div>
         </div>
         <div className="work-exp">
-          <div className="container">
-            <div className="work">
-              <h2>Work Experience</h2>
-              <p>
-                In my software-focused work experience, I have demonstrated
-                innovation and a keen adherence to deadlines. As a Software
-                Developer, I have engaged with designing and developing
-                user-centered digital/print marketing assets, overseeing the
-                entire process from inception to polished final products.
-              </p>
+      <div className="container">
+        <div className="work">
+          <h2>Work Experience</h2>
+          <p>
+            In my software-focused work experience, I have demonstrated
+            innovation and a keen adherence to deadlines. As a Software
+            Developer, I have engaged with designing and developing
+            user-centered digital/print marketing assets, overseeing the
+            entire process from inception to polished final products.
+          </p>
+        </div>
+        <div className="exp">
+          <div>
+            <h3>Junior Software Developer</h3>
+            <p>
+              Duties:
+            </p>
+            <ul>
+              <li>Designing and developing software applications.</li>
+              <li>Collaborating with cross-functional teams to meet project goals.</li>
+            </ul>
+            <h5>
+              Younglings Africa, Cape Town, Western Cape, RSA
+              <br />
+              <p>2023 - Present</p>
+            </h5>
+            <div className="pbox">
+              <p>18 Months</p>
             </div>
-            <div className="exp">
-              <div>
-                <h3>Junior Software Developer</h3>
-                <h5>
-                  Younglings Africa, Cape Town, Western Cape, RSA
-                  <br />
-                  <p>2023 - Present</p>
-                </h5>
-                <div className="pbox">
-                  <p>18 Months</p>
-                </div>
-                <hr />
-              </div>
-              <div>
-                <h3>System Development Learnership</h3>
-                <h5>
-                  CapaCiTi, Salt River, Cape Town, Western Cape, RSA
-                  <br />
-                  <p>Jan 2023 - July 2023</p>
-                </h5>
-                <div className="pbox">
-                  <p>5 Months</p>
-                </div>
-                <hr />
-              </div>
-              <div>
-                <h3>End User Computing Learnership</h3>
-                <h5>
-                  Multichoice, Bellville, Western Cape, RSA
-                  <br />
-                  <p>Jan 2022 - Dec 2022</p>
-                </h5>
-                <div className="pbox">
-                  <p>12 Months</p>
-                </div>
-                <hr />
-              </div>
-              <div>
-                <h3>IT TECHNICIAN INTERN</h3>
-                <h5>
-                  Masibambane Senior Secondary School, Kraaifontein, Western
-                  Cape, RSA
-                  <br />
-                  <p>Jan 2021 - Dec 2022</p>
-                </h5>
-                <div className="pbox">
-                  <p>12 Months</p>
-                </div>
-                <hr />
-              </div>
+            <hr />
+          </div>
+          <div>
+            <h3>System Development Learnership</h3>
+            <p>
+              Duties:
+            </p>
+            <ul>
+              <li>Gaining hands-on experience in system development practices.</li>
+              <li>Learning and applying software development best practices.</li>
+            </ul>
+            <h5>
+              CapaCiTi, Salt River, Cape Town, Western Cape, RSA
+              <br />
+              <p>Jan 2023 - July 2023</p>
+            </h5>
+            <div className="pbox">
+              <p>5 Months</p>
             </div>
+            <hr />
+          </div>
+          <div>
+            <h3>End User Computing Learnership</h3>
+            <p>
+              Duties:
+            </p>
+            <ul>
+              <li>Providing technical support to end users and resolving IT-related issues.</li>
+              <li>Participating in training programs to enhance IT skills.</li>
+            </ul>
+            <h5>
+              Multichoice, Bellville, Western Cape, RSA
+              <br />
+              <p>Jan 2022 - Dec 2022</p>
+            </h5>
+            <div className="pbox">
+              <p>12 Months</p>
+            </div>
+            <hr />
+          </div>
+          <div>
+            <h3>IT TECHNICIAN INTERN</h3>
+            <p>
+              Duties:
+            </p>
+            <ul>
+              <li>Providing technical support to school staff and students.</li>
+              <li>Maintaining and troubleshooting computer systems.</li>
+            </ul>
+            <h5>
+              Masibambane Senior Secondary School, Kraaifontein, Western
+              Cape, RSA
+              <br />
+              <p>Jan 2021 - Dec 2022</p>
+            </h5>
+            <div className="pbox">
+              <p>12 Months</p>
+            </div>
+            <hr />
           </div>
         </div>
       </div>
     </div>
+                <hr />
+              </div>
+            </div>
+      
   );
 };
 
